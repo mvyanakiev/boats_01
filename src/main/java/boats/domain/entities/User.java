ecee218d4613bd8d4e96cb390ee58b29c8b2574c
Milko Yanakiev <myanakiev@hotmail.com>
2019-03-12 18:38:05 +0200

    users added

---
+++ src/main/java/boats/domain/entities/User.java
@@ -0,0 +1,54 @@
+package boats.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+
+    public User() {
+    }
+
+    @Column(name = "username", nullable = false, updatable = false)
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "phone")
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
