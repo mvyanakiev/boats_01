f640be8797043b8bf705ba63a03d11f885d25b85
Milko Yanakiev <myanakiev@gmail.com>
2019-03-13 14:58:40 +0200

    added classes

---
+++ src/main/java/boats/config/ConfigVallues.java
@@ -0,0 +1,10 @@
+package boats.config;
+
+import java.math.BigDecimal;
+
+public class ConfigVallues {
+
+    public static final BigDecimal pricePeriodA = BigDecimal.valueOf(400);
+    public static final BigDecimal pricePeriodB = BigDecimal.valueOf(300);
+    public static final BigDecimal pricePeriodC = BigDecimal.valueOf(200);
+}
